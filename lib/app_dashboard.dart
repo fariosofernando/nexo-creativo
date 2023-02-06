9943deca3dda4079432ec15930c4bc91ed101aaa
farioso_fernando <farioso.f@gmail.com>
2023-02-06 20:08:50 +0200

    ref: tratando imagens

--- lib/app_dashboard.dart
+++ lib/app_dashboard.dart
@@ -27,93 +27,91 @@ class _AppDashboardState extends State<AppDashboard> {
   Widget build(BuildContext context) {
     Size mediaSize = MediaQuery.of(context).size;
 
-    return Scaffold(
-      body: GestureDetector(
-        onPanStart: (drag) {
-          AppAlerts.warnig(context);
-        },
-        child: Stack(children: [
-          Align(
-            alignment: Alignment.center,
-            child: PageView(
-              onPageChanged: (page) {
-                AppInstances.instance.globalStore.currentPage = page;
-              },
-              controller: _pageController,
-              scrollDirection: Axis.vertical,
-              pageSnapping: false,
-              children: (mediaSize.width > 780)
-                  ? [
-                      AppHomePage(
-                        mediaSize: mediaSize,
-                      ),
-                    ]
-                  : [
-                      AppHomePage(
-                        mediaSize: mediaSize,
-                      ),
-                      AppAboutPage(
-                        mediaSize: mediaSize,
-                      ),
-                      AppSkillsPage(
-                        mediaSize: mediaSize,
-                      ),
-                      AppServicesPage(
-                        mediaSize: mediaSize,
-                      ),
-                      AppPortfolioPage(
-                        mediaSize: mediaSize,
-                      ),
-                      AppContactmePage(
-                        mediaSize: mediaSize,
-                      ),
-                    ],
-            ),
-          ),
-          Align(
-            alignment: (mediaSize.width >= 800.0) ? Alignment.topCenter : Alignment.bottomCenter,
-            child: AppBottonBar((page) {
-              _pageController.animateToPage(page, duration: const Duration(milliseconds: 1200), curve: Curves.ease);
-            }),
-          ),
-          Visibility(
-            visible: (mediaSize.width >= 780),
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                width: mediaSize.width,
-                alignment: Alignment.centerLeft,
-                height: 35,
-                color: const Color(0xFFE1C648),
-                child: Row(
-                  children: const [
-                    SizedBox(width: 22.0),
-                    Icon(Icons.warning_amber_outlined),
-                    SizedBox(width: 8.0),
-                    Text('Acessa o website pelo seu celular para navegar. Não use um tablet para isso.', style: TextStyle(fontSize: 12.5)),
+    return GestureDetector(
+      onPanStart: (drag) {
+        AppAlerts.warnig(context);
+      },
+      child: Stack(children: [
+        Align(
+          alignment: Alignment.center,
+          child: PageView(
+            onPageChanged: (page) {
+              AppInstances.instance.globalStore.currentPage = page;
+            },
+            controller: _pageController,
+            scrollDirection: Axis.vertical,
+            // pageSnapping: false,
+            children: (mediaSize.width > 780)
+                ? [
+                    AppHomePage(
+                      mediaSize: mediaSize,
+                    ),
+                  ]
+                : [
+                    AppHomePage(
+                      mediaSize: mediaSize,
+                    ),
+                    AppAboutPage(
+                      mediaSize: mediaSize,
+                    ),
+                    AppSkillsPage(
+                      mediaSize: mediaSize,
+                    ),
+                    AppServicesPage(
+                      mediaSize: mediaSize,
+                    ),
+                    AppPortfolioPage(
+                      mediaSize: mediaSize,
+                    ),
+                    AppContactmePage(
+                      mediaSize: mediaSize,
+                    ),
                   ],
-                ),
+          ),
+        ),
+        Align(
+          alignment: (mediaSize.width >= 800.0) ? Alignment.topCenter : Alignment.bottomCenter,
+          child: AppBottonBar((page) {
+            _pageController.animateToPage(page, duration: const Duration(milliseconds: 1200), curve: Curves.ease);
+          }),
+        ),
+        Visibility(
+          visible: (mediaSize.width >= 780),
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: mediaSize.width,
+              alignment: Alignment.centerLeft,
+              height: 35,
+              color: const Color(0xFFE1C648),
+              child: Row(
+                children: const [
+                  SizedBox(width: 22.0),
+                  Icon(Icons.warning_amber_outlined),
+                  SizedBox(width: 8.0),
+                  Text('Acessa o website pelo seu celular para navegar. Não use um tablet para isso.', style: TextStyle(fontSize: 12.5)),
+                ],
               ),
             ),
           ),
-          Observer(builder: (_) {
-            return Visibility(
-              visible: (AppInstances.instance.globalStore.currentPage != 0),
-              child: Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 22.0, bottom: 60.0),
-                  child: FloatingActionButton(
-                      onPressed: () {
-                        _pageController.animateToPage(0, duration: const Duration(milliseconds: 1200), curve: Curves.ease);
-                      },
-                      child: const Icon(Icons.arrow_upward_rounded)),
-                ),
+        ),
+        Observer(builder: (_) {
+          return Visibility(
+            visible: (AppInstances.instance.globalStore.currentPage != 0),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 22.0, bottom: 60.0),
+                child: FloatingActionButton(
+                    onPressed: () {
+                      _pageController.animateToPage(0, duration: const Duration(milliseconds: 1200), curve: Curves.ease);
+                    },
+                    child: const Icon(Icons.arrow_upward_rounded)),
               ),
-            );
-          }),
-        ]),
-      ),
+            ),
+          );
+        }),
+      ]),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,34 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_web_plugins/url_strategy.dart' as url;
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'app_dashboard.dart';
 import 'app_instances.dart';
 import 'color_schemes.g.dart';
 
 void main() {
+  setUrlStrategy(PathUrlStrategy());
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // setUrlStrategy(PathUrlStrategy());
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     debugInvertOversizedImages = false;
 
-    return Observer(builder: (context) {
-      return MaterialApp(
-        title: 'Portfolio',
-        debugShowCheckedModeBanner: false,
-        theme: (AppInstances.instance.globalStore.darkModeActivated == false) ? ThemeData(useMaterial3: true, colorScheme: lightColorScheme) : null,
-        darkTheme: (AppInstances.instance.globalStore.darkModeActivated == true) ? ThemeData(useMaterial3: true, colorScheme: darkColorScheme) : null,
-        home: const AppDashboard(title: 'My Portifolio Dashboard Page'),
-      );
-    });
+    return MaterialApp(
+      title: 'Portfolio',
+      debugShowCheckedModeBanner: false,
+      theme: (AppInstances.instance.globalStore.darkModeActivated == false) ? ThemeData(useMaterial3: true, colorScheme: lightColorScheme) : null,
+      darkTheme: (AppInstances.instance.globalStore.darkModeActivated == true) ? ThemeData(useMaterial3: true, colorScheme: darkColorScheme) : null,
+      home: Scaffold(
+        body: Builder(builder: (context) {
+          return Observer(builder: (_) {
+            return const AppDashboard(title: 'My Portifolio Dashboard Page');
+          });
+        }),
+      ),
+    );
   }
 }
