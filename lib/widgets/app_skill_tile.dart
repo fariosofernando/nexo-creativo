9da81e171194574d616be7405a88ea311ffad0e8
farioso_fernando <farioso.f@gmail.com>
2023-02-13 13:53:26 +0200

    fix: corrigindo a theme do expander.

--- lib/widgets/app_skill_tile.dart
+++ lib/widgets/app_skill_tile.dart
@@ -1,7 +1,10 @@
 import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
-class AppSkillTile extends StatelessWidget {
+import '../app_instances.dart';
+
+class AppSkillTile extends StatefulWidget {
   final String tech, description;
   final Icon icon;
   final List<AppSkillItem> items;
@@ -15,34 +18,42 @@ class AppSkillTile extends StatelessWidget {
   });
 
   @override
+  State<AppSkillTile> createState() => _AppSkillTileState();
+}
+
+class _AppSkillTileState extends State<AppSkillTile> {
+  @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(bottom: 22.0),
-      child: ExpandablePanel(
-        theme: const ExpandableThemeData(
-          headerAlignment: ExpandablePanelHeaderAlignment.center,
-          tapBodyToCollapse: true,
-        ),
-        header: Row(
-          children: [
-            icon,
-            Text(
-              '  $tech',
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-        collapsed: Text(
-          description,
-          softWrap: true,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-        ),
-        expanded: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: items,
-        ),
-      ),
+      child: Observer(builder: (_) {
+        return ExpandablePanel(
+          theme: ExpandableThemeData(
+            headerAlignment: ExpandablePanelHeaderAlignment.center,
+            tapBodyToCollapse: true,
+            iconColor: AppInstances.instance.globalStore.darkModeActivated ? const Color(0xFFFFFBFF) : const Color(0xFF1D1B16),
+          ),
+          header: Row(
+            children: [
+              widget.icon,
+              Text(
+                '  ${widget.tech}',
+                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          collapsed: Text(
+            widget.description,
+            softWrap: true,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+          expanded: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: widget.items,
+          ),
+        );
+      }),
     );
   }
 }
